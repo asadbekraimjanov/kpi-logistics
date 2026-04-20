f49f9239f03b8ced5e637721243c72aa6f26a107
Victus <asadbekraimjonov09@gmail.com>
2026-04-20 06:59:42 +0500

    modified

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -23,7 +23,7 @@
                     <!--                     class="!h-10" alt="">-->
                     <el-icon v-if="isCollapse" @click="isCollapse = !isCollapse" size="24"><Expand /></el-icon>
                     <el-icon v-else @click="isCollapse = !isCollapse" size="24"><Fold /></el-icon>
-                    <p class="text-lg uppercase">Ish faoliyati samaradorligini monitoring qilish platformasi</p>
+                    <p class="text-lg font-semibold uppercase">Ish faoliyati samaradorligini monitoring qilish platformasi</p>
                 </div>
                 <el-avatar class="cursor-pointer"></el-avatar>
             </div>
